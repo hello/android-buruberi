7fdfb168b569061e9988411690c0f77df1073cc9
km <km@sayhello.com>
2015-10-29 10:53:21 -0700

    fix up copyright notices

--- core/src/main/java/is/hello/buruberi/bluetooth/errors/BondException.java
+++ core/src/main/java/is/hello/buruberi/bluetooth/errors/BondException.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2015 Hello, Inc
+ * Copyright 2015 Hello Inc.
  * Copyright (C) 2013 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
